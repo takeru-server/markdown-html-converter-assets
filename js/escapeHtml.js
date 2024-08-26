5a544ee36ba80d8b064668aaecd7396d1b07f100
takeru-server <108921150+takeru-server@users.noreply.github.com>
2024-08-26 18:49:45 +0900

    Update escapeHtml.js

--- js/escapeHtml.js
+++ js/escapeHtml.js
@@ -1,7 +1,7 @@
 // 標準入力から HTML を読み込む
 fso = new ActiveXObject("Scripting.FileSystemObject");
 stdin = fso.GetStandardStream(0);
-htmlContent = stdin.ReadAll;
+htmlContent = stdin.ReadAll();
 
 // HTML エスケープ処理と <script> タグの挿入
 escapedHtml = escapeHtmlAndInsertScripts(htmlContent);
@@ -12,11 +12,11 @@ stdout.WriteLine(escapedHtml);
 
 function escapeHtmlAndInsertScripts(html) {
   // <script>タグを生成 (複数行に分割)
-  jqueryScript = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">'
+  jqueryScript = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">';
   jqueryScript += '</scr' + 'ipt>';
-  copyButtonScript = '<script src="https://takeru-server.github.io/markdown-html-converter-assets/js/copy-button.js">'
+  copyButtonScript = '<script src="https://takeru-server.github.io/markdown-html-converter-assets/js/copy-button.js">';
   copyButtonScript += '</scr' + 'ipt>';
-  copyCodeButtonScript = '<script src="https://takeru-server.github.io/markdown-html-converter-assets/js/copy-code-button.js">'
+  copyCodeButtonScript = '<script src="https://takeru-server.github.io/markdown-html-converter-assets/js/copy-code-button.js">';
   copyCodeButtonScript += '</scr' + 'ipt>';
 
   // </body>タグの直前に挿入
