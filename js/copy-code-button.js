ede3237bbc7eca55212e3044d86d1bc44a05a9d8
takeru-server <108921150+takeru-server@users.noreply.github.com>
2024-08-26 10:46:27 +0900

    Update copy-code-button.js

--- js/copy-code-button.js
+++ js/copy-code-button.js
@@ -1,7 +1,7 @@
 // コピーボタン作成	Ver.2.0から、変換バッチから独立して使用する
 $(document).ready(function() {
 	$('pre code.sourceCode').each(function() {
-		const copyButton = $('<button class="copy-button">コピー</button><br>');
+		const copyButton = $('<button class="copy-button">コピー</button>');
 		copyButton.on('click', function() {
 			const codeBlock = $(this).parent().find('code');
 			const tempTextArea = $('<textarea>');
