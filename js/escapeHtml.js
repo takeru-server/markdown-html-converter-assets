a8d7081c8c60b147a7ccbb7bbd120eb388e4c3dd
takeru-server <108921150+takeru-server@users.noreply.github.com>
2024-08-26 16:43:37 +0900

    Update escapeHtml.js

--- js/escapeHtml.js
+++ js/escapeHtml.js
@@ -1,8 +1,28 @@
-document.body.innerHTML = (function escapeHtml(unsafe) {
-  return unsafe
-         .replace(/&/g, '&')
-         .replace(/</g, '<')
-         .replace(/>/g, '>')
-         .replace(/"/g, '"')
-         .replace(/'/g, ''');
-})(document.body.innerHTML);
+// bodyタグのコンテンツを取得
+var bodyContent = document.body.innerHTML;
+
+// bodyタグの中身を空にする
+document.body.innerHTML = '';
+
+// scriptタグを生成
+var jqueryScript = document.createElement('script');
+jqueryScript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js';
+var copyButtonScript = document.createElement('script');
+copyButtonScript.src = 'https://takeru-server.github.io/markdown-html-converter-assets/js/copy-button.js';
+var copyCodeButtonScript = document.createElement('script');
+copyCodeButtonScript.src = 'https://takeru-server.github.io/markdown-html-converter-assets/js/copy-code-button.js';
+
+// scriptタグをbodyタグに追加
+document.body.appendChild(jqueryScript);
+document.body.appendChild(copyButtonScript);
+document.body.appendChild(copyCodeButtonScript);
+
+// エスケープ処理を行ったbodyタグのコンテンツを追加
+document.body.innerHTML += (function escapeHtml(unsafe) {
+    return unsafe
+           .replace(/&/g, '&')
+           .replace(/</g, '<')
+           .replace(/>/g, '>')
+           .replace(/"/g, '"')
+           .replace(/'/g, ''');
+})(bodyContent);
