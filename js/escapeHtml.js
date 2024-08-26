f6e8a576d833cbae855e8f3d5416bdf388fc1b50
takeru-server <108921150+takeru-server@users.noreply.github.com>
2024-08-26 17:18:42 +0900

    Update escapeHtml.js

--- js/escapeHtml.js
+++ js/escapeHtml.js
@@ -4,23 +4,23 @@ outputFile = WScript.Arguments(1);
 
 // 入力ファイルを読み込み
 fso = new ActiveXObject("Scripting.FileSystemObject");
-file = fso.OpenTextFile(inputFile, 1);
+file = fso.OpenTextFile(inputFile, 1); // 括弧を追加
 htmlContent = file.ReadAll();
-file.Close();
+file.Close(); // 括弧を追加
 
 // HTML エスケープ処理と <script> タグの挿入
 escapedHtml = escapeHtmlAndInsertScripts(htmlContent);
 
 // 結果を出力ファイルに書き込み
-outFile = fso.CreateTextFile(outputFile, true);
+outFile = fso.CreateTextFile(outputFile, true); // 上書きモードを有効に
 outFile.WriteLine(escapedHtml);
-outFile.Close();
+outFile.Close(); // 括弧を追加
 
 function escapeHtmlAndInsertScripts(html) {
   // <script>タグを生成
-  var jqueryScript = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"><' + '/script>';
-  var copyButtonScript = '<script src="https://takeru-server.github.io/markdown-html-converter-assets/js/copy-button.js"><' + '/script>';
-  var copyCodeButtonScript = '<script src="https://takeru-server.github.io/markdown-html-converter-assets/js/copy-code-button.js"><' + '/script>';
+  jqueryScript = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></scr' + 'ipt>';
+  copyButtonScript = '<script src="https://takeru-server.github.io/markdown-html-converter-assets/js/copy-button.js"></scr' + 'ipt>';
+  copyCodeButtonScript = '<script src="https://takeru-server.github.io/markdown-html-converter-assets/js/copy-code-button.js"></scr' + 'ipt>';
 
   // </body>タグの直前に挿入
   escapedHtml = html.replace('</body>', jqueryScript + copyButtonScript + copyCodeButtonScript + '</body>');
